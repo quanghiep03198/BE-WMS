5ad954c30e4ee7c3075c3c1a6dd767566fd8084c
quanghiep03198 <quanghiep03198@gmail.com>
2024-09-06 13:25:14 +0700

    chore: update app config validator

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,3 +1,4 @@
+import { Languages } from '@/common/constants/global.enum'
 import { z } from 'zod'
 
 export enum Environment {
@@ -7,15 +8,35 @@ export enum Environment {
 
 export const configValidator = z.object({
 	NODE_ENV: z.nativeEnum(Environment),
-	PORT: z.string().transform((value) => Number(value)),
-	FALLBACK_LANGUAGE: z.enum(['en', 'vi', 'cn']),
-	DB_HOST: z.string(),
-	DB_USERNAME: z.string(),
-	DB_PASSWORD: z.string(),
-	DB_PORT: z.string(),
+	PORT: z
+		.string()
+		.trim()
+		.min(1)
+		.refine((value) => !isNaN(+value))
+		.transform((value) => +value),
+	FALLBACK_LANGUAGE: z.nativeEnum(Languages),
+	DB_HOST: z.string().trim().min(1),
+	DB_USERNAME: z.string().trim().min(1),
+	DB_PASSWORD: z.string().trim().min(1),
+	DB_PORT: z
+		.string()
+		.trim()
+		.min(1)
+		.refine((value) => !isNaN(+value))
+		.transform((value) => Number(value)),
 	DB_TRUST_SERVER_CERTIFICATE: z.string().transform((value) => Boolean(value) && JSON.parse(value)),
-	SALT_ROUND: z.string(),
-	JWT_SECRET: z.string()
+	REDIS_HOST: z.string().trim().min(1),
+	REDIS_PORT: z
+		.string()
+		.trim()
+		.min(1)
+		.refine((value) => value === '6379' || !isNaN(+value))
+		.transform((value) => Number(value)),
+	SALT_ROUND: z
+		.string()
+		.trim()
+		.min(1)
+		.refine((value) => !isNaN(+value))
+		.transform((value) => Number(value)),
+	JWT_SECRET: z.string().trim().min(1)
 })
-
-export const configuration = () => ({})
