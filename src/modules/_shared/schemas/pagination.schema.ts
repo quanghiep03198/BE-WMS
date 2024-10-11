f9973e2354050ac31c7186fdb5f31862c7bb97fb
quanghiep03198 <quanghiep03198@gmail.com>
2024-10-11 13:01:52 +0700

    chore: remove shared module

--- src/modules/_shared/schemas/pagination.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const paginateOptionValidator = z.object({
-	page: z.number().positive().default(1),
-	limit: z.number().positive().default(10)
-})
-
-export type PaginationOptionsDTO = z.infer<typeof paginateOptionValidator>
