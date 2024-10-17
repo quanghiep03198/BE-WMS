79cc23bc53f13353a91fadc5594a7b6d2556a074
quanghiep03198 <quanghiep03198@gmail.com>
2024-10-17 11:56:00 +0700

    fix(rfid): get exchangable orders by order actual as top priority

--- src/modules/rfid/sql/exchangable-epc.sql
+++ src/modules/rfid/sql/exchangable-epc.sql
@@ -1,16 +1,18 @@
 DECLARE @UnknownValue NVARCHAR(10) = 'Unknown';
 DECLARE @InternalEpcPattern NVARCHAR(10) = 'E28%';
 
-SELECT EPC_Code AS epc FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet
+SELECT EPC_Code AS epc
+FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet
 WHERE EPC_Code IN (
-	SELECT cust1.EPC_Code FROM DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust cust1,  DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust cust2
+	SELECT cust1.EPC_Code
+	FROM DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust cust1, DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust cust2
 	WHERE cust1.mo_no <> cust2.mo_no
-	AND cust1.mat_code = cust2.mat_code
-	AND cust1.mo_no IN ( SELECT TRIM(value) AS mo_no
-        FROM STRING_SPLIT(@0, ','))
+		AND cust1.mat_code = cust2.mat_code
+		AND COALESCE(cust1.mo_no_actual, cust1.mo_no) IN (SELECT TRIM(value) AS mo_no
+		FROM STRING_SPLIT(@0, ','))
 )
-AND EPC_Code NOT LIKE @InternalEpcPattern 
-AND mo_no <> @1
+	AND EPC_Code NOT LIKE @InternalEpcPattern
+	AND mo_no <> @1
 
 -- WITH split_mo_no AS 
 --     (
