4ea044b10595216b4bdfed2c72495d5c4171507c
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-07 21:35:56 +0700

    refactor(log): separate log files corresponding type of log

--- src/common/helpers/file-logger.helper.ts
+++ src/common/helpers/file-logger.helper.ts
@@ -3,23 +3,37 @@ import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 import { isPrimitive } from '../utils/common.util'
 
+type LogType = 'error' | 'debug' | 'log'
+
 export class FileLogger {
 	protected static dirPath: string = resolve('logs')
-	protected static filePath: string = resolve('logs/app.log')
+	protected static logFilePath: string = resolve('logs/logs.log')
+	protected static errorLogFilePath: string = resolve('logs/errors.log')
+	protected static debugLogFilePath: string = resolve('logs/debug.log')
 
 	public static initialize(): void {
 		if (!existsSync(this.dirPath)) {
 			mkdirSync(this.dirPath, { recursive: true })
 		}
-		if (!existsSync(this.filePath)) {
-			writeFileSync(this.filePath, '')
-		}
+		if (!existsSync(this.logFilePath)) writeFileSync(this.logFilePath, '')
+		if (!existsSync(this.errorLogFilePath)) writeFileSync(this.errorLogFilePath, '')
+		if (!existsSync(this.debugLogFilePath)) writeFileSync(this.debugLogFilePath, '')
 	}
 
-	protected static rewrite(log: string) {
-		const data = readFileSync(this.filePath).toString().split('\n')
+	protected static rewrite(type: LogType, log: string) {
+		const data = readFileSync(this.logFilePath).toString().split('\n')
 		data.splice(0, 0, log)
-		writeFileSync(this.filePath, data.join('\n'))
+		const filePath = (() => {
+			switch (type) {
+				case 'debug':
+					return this.debugLogFilePath
+				case 'error':
+					return this.errorLogFilePath
+				default:
+					return this.logFilePath
+			}
+		})()
+		writeFileSync(filePath, data.join('\n'))
 	}
 
 	protected static format(type: 'Error' | 'Debug' | 'Log', ctx: any) {
@@ -30,14 +44,14 @@ export class FileLogger {
 	}
 
 	public static error(e: Error) {
-		this.rewrite(this.format('Error', e.stack))
+		this.rewrite('error', this.format('Error', e.stack))
 	}
 
 	public static debug(arg: any) {
-		this.rewrite(this.format('Debug', arg))
+		this.rewrite('debug', this.format('Debug', arg))
 	}
 
 	public static log(arg: any) {
-		this.rewrite(this.format('Log', arg))
+		this.rewrite('log', this.format('Log', arg))
 	}
 }
