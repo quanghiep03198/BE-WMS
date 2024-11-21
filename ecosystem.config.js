dc42b51cf0c6fa8a3b606b94670758c6ea255147
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-21 10:38:31 +0700

    chore: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,39 @@
+/**
+ * @typedef {Object} AppConfig
+ * @property {string} name
+ * @property {string} script
+ * @property {string} cwd
+ * @property {boolean} autorestart
+ * @property {boolean} watch
+ * @property {string} exec_mode
+ * @property {string} max_memory_restart
+ * @property {string} cron_restart
+ * @property {string} error_file
+ */
+
+/**
+ * @typedef {Object} DeployConfig
+ * @property {string} user
+ */
+
+/**
+ * @typedef {Object} EcosystemConfig
+ * @property {AppConfig[]} apps
+ * @property {Object.<string, DeployConfig>} deploy
+ */
+
+/** @type {EcosystemConfig} */
 module.exports = {
 	apps: [
 		{
 			name: 'wms-api',
 			script: './dist/main.js',
 			cwd: __dirname,
-			autorestart: true,
+			autorestart: false,
 			watch: false,
 			exec_mode: 'cluster',
-			max_memory_restart: '500M'
+			max_memory_restart: '500M',
+			cron_restart: '0 */24 * * *'
 		}
 	]
 }
