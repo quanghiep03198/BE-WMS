6f6f65cc917afedfe90f03d656f6f378fa41414a
quanghiep03198 <quanghiep03198@gmail.com>
2024-11-27 16:06:43 +0700

    chore(base)!: update base abstract entity column constraint

--- src/modules/_base/base.abstract.entity.ts
+++ src/modules/_base/base.abstract.entity.ts
@@ -7,27 +7,27 @@ export abstract class BaseAbstractEntity {
 	@PrimaryGeneratedColumn({ name: 'keyid', type: 'int' })
 	id: number
 
-	@Column({ type: 'nvarchar', length: 50, default: null })
-	user_code_created: string
+	@Column({ type: 'nvarchar', length: 50, nullable: true })
+	user_code_created: string | null
 
-	@Column({ type: 'nvarchar', length: 50, default: null })
-	user_name_created: string
+	@Column({ type: 'nvarchar', length: 50, nullable: true })
+	user_name_created: string | null
 
-	@Column({ type: 'nvarchar', length: 50, default: null })
-	user_code_updated: string
+	@Column({ type: 'nvarchar', length: 50, nullable: true })
+	user_code_updated: string | null
 
-	@Column({ type: 'nvarchar', length: 50, default: null })
-	user_name_updated: string
+	@Column({ type: 'nvarchar', length: 50, nullable: true })
+	user_name_updated: string | null
 
 	@CreateDateColumn({ type: 'datetime', default: format(new Date(), 'yyyy-MM-dd HH:mm:ss') })
 	created: Date
 
-	@UpdateDateColumn({ type: 'datetime' })
+	@UpdateDateColumn({ type: 'datetime', nullable: true, onUpdate: 'CURRENT_TIMESTAMP' })
 	updated: Date
 
 	@Column({ name: 'isactive', type: 'varchar', length: 1, enum: RecordStatus, default: RecordStatus.ACTIVE })
 	is_active: RecordStatus
 
-	@Column({ type: 'nvarchar', length: 255, default: null })
-	remark: string
+	@Column({ type: 'nvarchar', length: 255, nullable: true })
+	remark: string | null
 }
