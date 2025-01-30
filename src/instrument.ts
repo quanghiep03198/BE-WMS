7065872e31f39ced87453afc2973a25ff106cd04
quanghiep03198 <quanghiep03198@gmail.com>
2025-01-30 09:50:11 +0700

    feat: enable Sentry integration only in production environment

--- src/instrument.ts
+++ src/instrument.ts
@@ -5,5 +5,6 @@ import env from './common/utils/env.util'
 Sentry.init({
 	dsn: env('SENTRY_DSN'),
 	integrations: [nodeProfilingIntegration()],
-	tracesSampleRate: 1.0
+	tracesSampleRate: 1.0,
+	enabled: env('NODE_ENV') === 'production'
 })
