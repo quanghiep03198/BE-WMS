0848ce6520f14ef82caf80a8617b8f9df4676fd8
quanghiep03198 <quanghiep03198@gmail.com>
2025-02-27 16:43:13 +0700

    feat: outbound report detail by size

--- src/modules/report/sql/outbound-report.sql
+++ src/modules/report/sql/outbound-report.sql
@@ -1,48 +1,96 @@
+-- WITH datalist AS (
+-- 	SELECT EPC_Code,
+-- 		mo_no,
+-- 		mo_no_actual,
+-- 		rfid_status,
+-- 		record_time, 
+-- 		dept_name, 
+-- 		stationNO 
+-- 	FROM dv_InvRFIDrecorddet
+-- 	UNION ALL 
+-- 	SELECT EPC_Code,
+-- 		mo_no,
+-- 		mo_no_actual,
+-- 		rfid_status,
+-- 		record_time, 
+-- 		dept_name, 
+-- 		stationNO 
+-- 	FROM dv_InvRFIDrecorddet_backup_Daily
+-- )
+-- SELECT 
+-- 	COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown') AS mo_no,
+-- 	match.mat_code,
+-- 	match.shoestyle_codefactory AS shoes_style_code_factory,
+-- 	CAST(manf.mo_sumqty AS INT) AS order_qty,
+-- 	COUNT(DISTINCT inv.EPC_Code) AS outbound_qty,
+-- 	CAST(inv.record_time AS DATE) AS outbound_date, 
+-- 	CASE WHEN COUNT(inv.mo_no_actual) > 0 THEN 1 ELSE 0 END AS is_exchanged
+-- FROM datalist inv
+-- LEFT JOIN dv_rfidmatchmst_cust match
+-- 	ON 
+-- 		inv.EPC_Code = match.EPC_Code 
+-- 		AND COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown') = COALESCE(match.mo_no_actual, match.mo_no, 'Unknown')
+-- LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst manf
+-- 	ON manf.mo_no = COALESCE(inv.mo_no_actual, inv.mo_no)
+-- WHERE 
+-- 	inv.rfid_status = 'B'
+-- 	AND inv.EPC_Code NOT LIKE '303429%'
+-- 	AND inv.EPC_Code NOT LIKE 'E28%'
+-- 	AND COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown') NOT IN ('13D05B006')
+-- 	AND inv.stationNO LIKE 'CUS%WH103'
+-- 	AND CAST(inv.record_time AS DATE) = CAST(@0 AS DATE)
+-- GROUP BY 
+-- 	COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown'),
+-- 	manf.mo_sumqty,
+-- 	inv.dept_name,
+-- 	CAST(inv.record_time AS DATE),
+-- 	match.mat_code,
+-- 	match.shoestyle_codefactory
+
 WITH datalist AS (
-	SELECT EPC_Code,
-		mo_no,
-		mo_no_actual,
-		rfid_status,
-		record_time, 
-		dept_name, 
-		stationNO 
-	FROM dv_InvRFIDrecorddet
-	UNION ALL 
-	SELECT EPC_Code,
-		mo_no,
-		mo_no_actual,
-		rfid_status,
-		record_time, 
-		dept_name, 
-		stationNO 
-	FROM dv_InvRFIDrecorddet_backup_Daily
+   SELECT EPC_Code, mo_no, mo_no_actual, rfid_status, record_time,stationNO, FC_server_code, dept_name
+   FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet
+   UNION ALL
+   SELECT EPC_Code, mo_no, mo_no_actual, rfid_status, record_time,stationNO, FC_server_code, dept_name
+   FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily 
+),
+accumulated_counts AS (
+    SELECT mo_no, COUNT(DISTINCT EPC_Code) AS accumulated_inbound_qty
+    FROM datalist
+    GROUP BY mo_no
 )
-SELECT 
-	COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown') AS mo_no,
-	match.mat_code,
-	match.shoestyle_codefactory AS shoes_style_code_factory,
-	CAST(manf.mo_sumqty AS INT) AS order_qty,
-	COUNT(DISTINCT inv.EPC_Code) AS outbound_qty,
-	CAST(inv.record_time AS DATE) AS outbound_date, 
-	CASE WHEN COUNT(inv.mo_no_actual) > 0 THEN 1 ELSE 0 END AS is_exchanged
+SELECT
+	ISNULL(inv.mo_no, 'Unknown') AS mo_no,
+	ISNULL(match.mat_code, 'Unknown') AS mat_code,
+	ISNULL(match.shoestyle_codefactory,'Unknown')AS shoes_style_code_factory,
+	ISNULL(prod.mat_ecolor, 'Unknown') AS mat_ecolor,
+    inv.FC_server_code AS factory_code,
+	CAST(ISNULL(manf.mo_sumqty, 0) AS INT) AS order_qty,
+	ac.accumulated_inbound_qty,
+	COUNT(DISTINCT inv.EPC_Code) AS daily_outbound_qty,
+	(manf.mo_sumqty - COALESCE(ac.accumulated_inbound_qty, 0)) AS missing_qty
 FROM datalist inv
-LEFT JOIN dv_rfidmatchmst_cust match
-	ON 
-		inv.EPC_Code = match.EPC_Code 
-		AND COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown') = COALESCE(match.mo_no_actual, match.mo_no, 'Unknown')
-LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst manf
+LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust match 
+	ON inv.EPC_Code = match.EPC_Code
+LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst manf 
 	ON manf.mo_no = COALESCE(inv.mo_no_actual, inv.mo_no)
+LEFT JOIN wuerp_vnrd.dbo.ta_productmst prod 
+	ON match.mat_code = prod.mat_code
+LEFT JOIN accumulated_counts ac
+    ON ac.mo_no = inv.mo_no
 WHERE 
 	inv.rfid_status = 'B'
-	AND inv.EPC_Code NOT LIKE '303429%'
-	AND inv.EPC_Code NOT LIKE 'E28%'
-	AND COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown') NOT IN ('13D05B006')
 	AND inv.stationNO LIKE 'CUS%WH103'
-	AND CAST(inv.record_time AS DATE) = CAST(@0 AS DATE)
+	AND inv.EPC_Code NOT LIKE '303429%' 
+	AND inv.EPC_Code NOT LIKE 'E28%'
+	AND COALESCE(inv.mo_no_actual, inv.mo_no) <> '13D05B006'
+	AND CAST(inv.record_time AS DATE) = @0
 GROUP BY 
-	COALESCE(inv.mo_no_actual, inv.mo_no, 'Unknown'),
-	manf.mo_sumqty,
-	inv.dept_name,
-	CAST(inv.record_time AS DATE),
-	match.mat_code,
-	match.shoestyle_codefactory
+   inv.mo_no,
+   prod.mat_ecolor,
+   manf.mo_sumqty,
+   ac.accumulated_inbound_qty,
+   match.mat_code,
+   match.shoestyle_codefactory,
+   inv.FC_server_code
+ORDER BY inv.mo_no DESC;
---
+++ src/modules/report/sql/outbound-size-qty-report.sql
@@ -0,0 +1,23 @@
+WITH datalist AS (
+	SELECT EPC_Code, mo_no, record_time, FC_server_code, stationNO FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet
+	WHERE stationNO LIKE 'CUS%WH103'
+		AND rfid_status = 'B'
+	UNION ALL 
+	SELECT EPC_Code, mo_no, record_time, FC_server_code, stationNO 
+	FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily
+	WHERE stationNO LIKE 'CUS%WH103'
+		AND rfid_status = 'B'
+)
+SELECT 
+	ISNULL(c.size_numcode, 'Unknown') AS size_numcode, 
+	COUNT(DISTINCT d.EPC_Code) AS inbound_qty
+FROM datalist d 
+LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust c
+	ON d.EPC_Code = c.EPC_Code
+WHERE d.mo_no = @0 
+	AND d.FC_server_code = @1
+   AND CAST(d.record_time AS DATE) = CAST(@2 AS DATE)
+   AND d.EPC_Code NOT LIKE '303429%'
+   AND d.EPC_Code NOT LIKE 'E28%'
+GROUP BY d.mo_no, ISNULL(c.size_numcode, 'Unknown'), d.FC_server_code, d.stationNO
+ORDER BY ISNULL(size_numcode, 'Unknown')
