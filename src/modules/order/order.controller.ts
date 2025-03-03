3a5b0a4b35943d2976c3c40488367d7059c7b5f8
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-03 17:05:09 +0700

    feat: combine customer epc

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common'
+import { Api, HttpMethod } from '@/common/decorators'
+import { Controller, DefaultValuePipe, Headers, NotFoundException, Param, Query } from '@nestjs/common'
+import { groupBy } from 'lodash'
+import { OrderService } from './order.service'
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+	constructor(private readonly orderService: OrderService) {}
+
+	@Api({
+		endpoint: '/search',
+		method: HttpMethod.GET
+	})
+	async searchCommandNumber(
+		@Headers('X-User-Company') factoryCode: string,
+		@Query('q', new DefaultValuePipe('')) searchTerm: string
+	) {
+		return await this.orderService.searchCommandNumber(factoryCode, searchTerm)
+	}
+
+	@Api({
+		endpoint: '/detail/:commandNumber',
+		method: HttpMethod.GET
+	})
+	async getOrderDetail(@Param('commandNumber') commandNumber: string) {
+		const [orders, sizeRun] = await Promise.all([
+			this.orderService.getCustOrderByCommandNumber(commandNumber),
+			this.orderService.getSizeRunByCommandNumber(commandNumber)
+		])
+
+		if (orders.length === 0) throw new NotFoundException('Command number could not be found')
+
+		const groupedOrders = groupBy(orders, 'mo_no')
+		const orderInformation = groupedOrders[commandNumber]
+
+		return {
+			mat_code: orderInformation[0].mat_code,
+			shoes_style_code_factory: orderInformation[0].shoes_style_code_factory,
+			mo_noseqs: orderInformation.map((item) => item.mo_noseq),
+			sizes: sizeRun
+		}
+	}
+}
--- src/modules/order/sql/order-information.sql
+++ src/modules/order/sql/order-information.sql
@@ -1,4 +1,4 @@
-SELECT TOP 1
+SELECT 
 	a.mo_no AS mo_no,
 	a.mat_code AS mat_code,
 	b.mo_noseq AS mo_noseq,
@@ -12,11 +12,10 @@ FROM wuerp_vnrd.dbo.ta_manufacturmst a
 	LEFT JOIN wuerp_vnrd.dbo.ta_manufacturdet b ON a.mo_no=b.mo_no AND b.isactive='Y'
 	LEFT JOIN wuerp_vnrd.dbo.ta_brand c ON c.custbrand_id = a.custbrand_id AND c.isactive = 'Y'
 	LEFT JOIN wuerp_vnrd.dbo.ta_ordermst d ON d.or_no = b.or_no AND d.isactive = 'Y'
-	LEFT JOIN wuerp_vnrd.dbo.ta_orderdet e ON e.or_no = d.or_no AND e.isactive = 'Y'
 	LEFT JOIN wuerp_vnrd.dbo.ta_productmst f ON f.mat_code= a.mat_code AND f.isactive= 'Y'
 	LEFT JOIN wuerp_vnrd.dbo.ta_shoefactorymst g ON g.shoestyle_systemcodefty = f.shoestyle_systemcodefty AND g.isactive = 'Y'
 	LEFT JOIN wuerp_vnrd.dbo.ta_ordersizerun h ON h.or_no = b.or_no AND h.isactive= 'Y'
 	LEFT JOIN wuerp_vnrd.dbo.ta_shoestylecolor i ON i.shoestyle_templink = f.shoestyle_templink and i.isactive = 'Y'
 	LEFT JOIN wuerp_vnrd.dbo.ta_ordersizerun k ON k.or_no = d.or_no AND k.isactive = 'Y'
-WHERE a.mo_no = @0
-ORDER BY b.mo_noseq DESC
+WHERE a.isactive = 'Y' AND a.mo_no = @0
+ORDER BY b.mo_noseq DESC, a.created DESC
