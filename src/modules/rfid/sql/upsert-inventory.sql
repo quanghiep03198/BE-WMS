82d0e7f551e633c7548eed323f2d76ee2e53fec0
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-05 11:15:40 +0700

    fix(rfid): update inventory upsert to set created timestamp on match

--- src/modules/rfid/sql/upsert-inventory.sql
+++ src/modules/rfid/sql/upsert-inventory.sql
@@ -6,12 +6,14 @@ USING (
    quantity, storage, FC_server_code, dept_code, dept_name
 )
 ON target.EPC_Code = source.EPC_Code
+WHEN MATCHED THEN
+   UPDATE SET created = GETDATE()
 WHEN NOT MATCHED THEN
-INSERT (
-   EPC_Code, mo_no, rfid_status, rfid_use, record_time, stationNO,
-   quantity, storage, FC_server_code, dept_code, dept_name
-)
-VALUES (
-   source.EPC_Code, source.mo_no, source.rfid_status, source.rfid_use, CAST(source.record_time AS DATETIME), source.stationNO,
-   source.quantity, source.storage, source.FC_server_code, source.dept_code, source.dept_name
-);
+   INSERT (
+      EPC_Code, mo_no, rfid_status, rfid_use, record_time, stationNO,
+      quantity, storage, FC_server_code, dept_code, dept_name
+   )
+   VALUES (
+      source.EPC_Code, source.mo_no, source.rfid_status, source.rfid_use, CAST(source.record_time AS DATETIME), source.stationNO,
+      source.quantity, source.storage, source.FC_server_code, source.dept_code, source.dept_name
+   );
