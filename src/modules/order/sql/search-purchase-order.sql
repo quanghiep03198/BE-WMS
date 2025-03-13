cc9b9b8e82074fa98fe37257a19ca2a909b0e540
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-13 15:49:42 +0700

    fix(order): update SQL query to use fully qualified table name for brand selection

--- src/modules/order/sql/search-purchase-order.sql
+++ src/modules/order/sql/search-purchase-order.sql
@@ -2,7 +2,7 @@ WITH CTE AS (
    SELECT IIF(ISNULL(or_custpoone,'')='', or_custpo, or_custpoone)[po] 
    FROM wuerp_vnrd.dbo.ta_ordermst
    WHERE custbrand_id IN (
-      SELECT custbrand_id FROM ta_brand WHERE brand_code IN ('TV','KB','UG')
+      SELECT custbrand_id FROM wuerp_vnrd.dbo.ta_brand WHERE brand_code IN ('TV','KB','UG')
    )
 ) 
 SELECT TOP 5 po from CTE
