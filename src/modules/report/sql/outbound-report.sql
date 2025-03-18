3d1631e7fc72bf03c2c3895c28eb008a70a300d8
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-18 08:15:55 +0700

    fix(report): refine SQL query for outbound report to filter by rfid_status and station number

--- src/modules/report/sql/outbound-report.sql
+++ src/modules/report/sql/outbound-report.sql
@@ -1,23 +1,32 @@
 WITH datalist AS (
-   SELECT EPC_Code, mo_no, mo_no_actual, rfid_status, record_time, stationNO, FC_server_code, dept_name
+   SELECT EPC_Code, mo_no, rfid_status, record_time, stationNO, FC_server_code, dept_name
    FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet
+   WHERE
+	 	rfid_status = 'B'
+		AND stationNO LIKE 'CUS%WH103'
+		AND EPC_Code NOT LIKE '303429%' 
+		AND EPC_Code NOT LIKE 'E28%'
+		AND mo_no <> '13D05B006'
    UNION ALL
-   SELECT EPC_Code, mo_no, mo_no_actual, rfid_status, record_time, stationNO, FC_server_code, dept_name
+   SELECT EPC_Code, mo_no, rfid_status, record_time, stationNO, FC_server_code, dept_name
    FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily 
+	WHERE
+	 	rfid_status = 'B'
+		AND stationNO LIKE 'CUS%WH103'
+		AND EPC_Code NOT LIKE '303429%' 
+		AND EPC_Code NOT LIKE 'E28%'
+		AND mo_no <> '13D05B006'
 ),
 accumulated_counts AS (
-    SELECT mo_no, COUNT(DISTINCT EPC_Code) AS accumulated_qty
-    FROM datalist
-    WHERE 
-        rfid_status = 'B'
-        AND stationNO LIKE 'CUS%WH103'
-    GROUP BY mo_no
+	SELECT mo_no, COUNT(DISTINCT EPC_Code) AS accumulated_qty
+	FROM datalist
+	GROUP BY mo_no
 )
 SELECT
 	ISNULL(inv.mo_no, 'Unknown') AS mo_no,
 	ISNULL(match.shoestyle_codefactory, 'Unknown') AS shoes_style_code_factory,
 	ISNULL(prod.mat_ecolor, 'Unknown') AS mat_ecolor,
-    inv.FC_server_code AS factory_code,
+   inv.FC_server_code AS factory_code,
 	CAST(ISNULL(manf.mo_sumqty, 0) AS INT) AS order_qty,
 	ac.accumulated_qty,
 	COUNT(DISTINCT inv.EPC_Code) AS daily_outbound_qty,
@@ -30,14 +39,8 @@ LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst manf
 LEFT JOIN wuerp_vnrd.dbo.ta_productmst prod 
 	ON match.mat_code = prod.mat_code
 LEFT JOIN accumulated_counts ac
-    ON ac.mo_no = inv.mo_no
-WHERE
-	inv.rfid_status = 'B'
-	AND inv.stationNO LIKE 'CUS%WH103'
-	AND inv.EPC_Code NOT LIKE '303429%' 
-	AND inv.EPC_Code NOT LIKE 'E28%'
-	AND COALESCE(inv.mo_no_actual, inv.mo_no) <> '13D05B006'
-	AND CAST(inv.record_time AS DATE) = @0
+   ON ac.mo_no = inv.mo_no
+WHERE CAST(inv.record_time AS DATE) = @0
 GROUP BY 
    inv.mo_no,
    prod.mat_ecolor,
