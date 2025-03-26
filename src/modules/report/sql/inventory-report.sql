2ebd31a330b9d93d58a09b1ec7d4684e3aa9cbb4
quanghiep03198 <quanghiep03198@gmail.com>
2025-03-26 17:31:20 +0700

    refactor(report): optimize inventory report SQL query for improved clarity and performance

--- src/modules/report/sql/inventory-report.sql
+++ src/modules/report/sql/inventory-report.sql
@@ -1,38 +1,46 @@
-SELECT 
-   a.inv_yearmonth, --month
-   a.brand_name, --Brand
-   a.shoestyle_cofactory AS shoes_style_code_factory, -- Factory Shoe style factory code
-   a.mo_no, -- Command number
-   CAST(ISNULL(b.mo_sumqty, 0) AS INT) AS order_qty,
-   a.or_no, --Order, 
-   a.po, -- Purchase order code
-   SUM(a.inv_initialqty) AS init_inv_qty, --TOTAL OF Beginning Inventory
-   SUM(a.inv_istotalqty) AS total_instock_qty, -- TOTAL OF Inventory In 
-   SUM(a.inv_ostotalqty) AS total_outstock_qty, -- TOTAL OF Inventory Out
-   SUM(a.inv_istotalqty) - SUM(a.inv_ostotalqty) AS actual_inv_qty, -- TOTAL OF Inventory Out
-   SUM(a.inv_finalqty) AS final_inv_qty, --TOTAL OF Ending Inventory
-   ('['+
-      STRING_AGG(
-         '{"size_numcode": ' + '"'+ a.size_numcode+ '"'  --Size
-         + ',"init_inv_qty": ' + CAST(CAST(a.inv_initialqty AS INT) AS NVARCHAR(50)) + -- Beginning Inventory
-         + ',"instock_qty": ' + CAST(CAST(a.inv_istotalqty AS INT)  AS NVARCHAR(50)) + -- Inventory In
-         + ',"outstock_qty": ' + CAST(CAST(a.inv_ostotalqty AS INT) AS NVARCHAR(50)) + -- Inventory Out
-         + ',"final_inv_qty": '+ CAST(CAST(a.inv_finalqty AS INT) AS NVARCHAR(50)) + -- Ending Inventory
-         '}', ',') 
-      WITHIN GROUP (ORDER BY RIGHT('0000'+IIF(CHARINDEX('.', a.size_numcode) > 0, a.size_numcode, a.size_numcode + '.0'), 5) ASC)
-   +']') AS size_data
-FROM DV_DATA_LAKE.dbo.dv_invprodmst a
-LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst b ON a.mo_no = b.mo_no
+SELECT DISTINCT
+  a.inv_yearmonth --month
+, a.brand_name --Brand
+, a.shoestyle_cofactory -- Factory Type 工廠型體
+, a.cust_shoestyle -- 客戶型體
+, a.mo_no --
+, a.po
+, SUM(a.mo_qty)[mo_qty] --TOTAL OF mo_qty 指令數量
+, SUM(a.inv_initialqty)[init_inv_qty] --TOTAL OF Beginning Inventory 期初庫存
+, (SUM(a.inv_istotalqty) + SUM(inv_manualqty))[total_instock_qty] -- TOTAL OF Inventory In 入庫
+, (SUM(a.inv_ostotalqty) + SUM(inv_manualqtyout))[total_outstock_qty] -- TOTAL OF Inventory Out 出庫
+, (SUM(inv_manualqty)) - (SUM(inv_manualqtyout))[actual_inv_qty] -- TOTAL OF Inventory Out 盤盈
+, SUM(a.inv_finalqty)[final_inv_qty] --TOTAL OF Ending Inventory 期末數量
+, CAST('['+
+   STRING_AGG(
+      '{' 
+         +'"size":' + '"' + a.size_numcode + '"' --Size
+         +',"ms_qty":' + CAST(CAST(a.mo_qty AS INT) AS NVARCHAR(50)) + --指令數量 mo_qty of current size
+         +',"int_qty":' + CAST(CAST(a.inv_initialqty AS INT) AS NVARCHAR(50)) + --Beginning Inventory 期初庫存
+         +',"ist_qty":' + CAST(CAST(a.inv_istotalqty AS INT)  AS NVARCHAR(50)) + --Inventory In 入庫
+         +',"mn_ist_qty":' + CAST(CAST(a.inv_manualqty AS INT)  AS NVARCHAR(50)) + --Manual Inventory InManual  手填入庫
+         +',"ost_qty":' +CAST(CAST(a.inv_ostotalqty AS INT) AS NVARCHAR(50)) + --Inventory Out 出庫
+         +',"mn_ost_qty":' + CAST(CAST(a.inv_manualqtyout AS INT) AS NVARCHAR(50)) + --Manual Inventory Out 手填出庫
+         +',"fnl_qty":' +CAST(CAST(ISNULL(a.inv_finalqty,0) AS INT) AS NVARCHAR(50)) + --Ending Inventory 期末數量
+      '}',
+      ','
+   ) 
+   WITHIN GROUP (
+      ORDER BY RIGHT('0000' + IIF(CHARINDEX('.', a.size_numcode) > 0, a.size_numcode,a.size_numcode + '.0'), 5) ASC
+   ) 
+   + ']' AS NVARCHAR(MAX)) AS size_data
+, a.remark
+FROM [dbo].[dv_invprodmst] a
 WHERE 
-   a.inv_yearmonth = @0
-   AND a.isactive='Y' 
+   a.isactive='Y' 
    AND a.inv_type='FG'
+   AND a.inv_yearmonth = @0
 GROUP BY
-   a.inv_yearmonth, 
-   a.brand_name, 
-   a.shoestyle_cofactory, 
-   a.po,
-   a.mo_no, 
-   a.or_no,
-   CAST(ISNULL(b.mo_sumqty, 0) AS INT)
-
+   a.inv_yearmonth
+   ,a.brand_name
+   ,a.shoestyle_cofactory
+   ,a.mo_no
+   ,a.remark
+   ,a.inv_type
+   ,a.cust_shoestyle
+   ,a.po
