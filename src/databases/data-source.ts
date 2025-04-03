f154ed237b5a8736af68591d291fc414c58ad3e8
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-03 18:28:39 +0700

    refactor(data-source): simplify database host configuration and remove unnecessary validations

--- src/databases/data-source.ts
+++ src/databases/data-source.ts
@@ -1,26 +1,11 @@
 import { env } from '@/common/utils'
-import { Logger } from '@nestjs/common'
 import 'dotenv/config'
-import { isIP } from 'net'
 import { join } from 'path'
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { type SeederOptions } from 'typeorm-extension'
 
-const logger = new Logger('TypeORM')
-
-const DB_HOST = env('SEEDING_DB_HOST') || env('SEEDING_DB_HOST')
-
-if (!DB_HOST) {
-	logger.error('Please provide a database host')
-	process.exit()
-}
-if (isIP(DB_HOST.trim()) === 0) {
-	logger.error('Please provide a valid IP address for the database host')
-	process.exit()
-}
-
 export default new DataSource({
-	host: DB_HOST,
+	host: env('DB_HOST'),
 	port: env('DB_PORT', { serialize: (value) => parseInt(value) }),
 	type: env('DB_TYPE'),
 	username: env('DB_USERNAME'),
