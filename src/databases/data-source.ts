b6a5d7b91052c0dc0aadffd09acfd9e0f60cf4ea
quanghiep03198 <quanghiep03198@gmail.com>
2025-04-03 18:37:29 +0700

    fix(data-source): add validation for database host and ensure it's a valid IP address

--- src/databases/data-source.ts
+++ src/databases/data-source.ts
@@ -1,11 +1,27 @@
 import { env } from '@/common/utils'
+import { Logger } from '@nestjs/common'
 import 'dotenv/config'
+import { isIP } from 'net'
 import { join } from 'path'
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { type SeederOptions } from 'typeorm-extension'
 
+const logger = new Logger('TypeORM')
+
+const DB_HOST = env('SEEDING_DB_HOST') || env('MIGRATION_DB_HOST') || env('DB_HOST')
+
+if (!DB_HOST) {
+	logger.error('Please provide a database host')
+	process.exit()
+}
+
+if (isIP(DB_HOST.trim()) === 0) {
+	logger.error('Please provide a valid IP address for the database host')
+	process.exit()
+}
+
 export default new DataSource({
-	host: env('DB_HOST'),
+	host: DB_HOST,
 	port: env('DB_PORT', { serialize: (value) => parseInt(value) }),
 	type: env('DB_TYPE'),
 	username: env('DB_USERNAME'),
