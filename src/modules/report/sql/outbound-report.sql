e9c3710c259d77c5ad2a905f6d79852be65c13a9
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-01 01:25:34 +0700

    refactor(outbound-report): replace temporary table with CTE for improved query performance

--- src/modules/report/sql/outbound-report.sql
+++ src/modules/report/sql/outbound-report.sql
@@ -1,39 +1,11 @@
 DECLARE @FallbackValue NVARCHAR(10) = 'Unknown';
 
-DROP TABLE IF EXISTS #datalist;
-
--- * Create temporary table to store data
-SELECT
-	EPC_Code,
-	COALESCE(po, @FallbackValue) AS po,
-	COALESCE(mo_no, @FallbackValue) AS mo_no,
-	COALESCE(shoestyle_codefactory, @FallbackValue) AS shoestyle_codefactory,
-	COALESCE(mat_ecolor, @FallbackValue) AS mat_ecolor,
-	COALESCE(size_code, @FallbackValue) AS size_numcode,
-	rfid_status,
-	record_time,
-	stationNO,
-	FC_server_code
-INTO #datalist
-FROM (
+WITH CTE AS (
 	SELECT i.EPC_Code, i.po, i.mo_no, i.size_code, i.rfid_status, i.record_time, i.stationNO, i.FC_server_code, r.shoestyle_codefactory, p.mat_ecolor
-	FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet i
-	LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust r
+	FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily i WITH (NOLOCK)
+	LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust r WITH (NOLOCK)
 		ON i.EPC_Code = r.EPC_Code
-	LEFT JOIN wuerp_vnrd.dbo.ta_productmst p
-		ON p.mat_code = r.mat_code AND p.isactive = 'Y'
-	WHERE
-		i.rfid_status = 'B'
-		AND i.EPC_Code NOT LIKE '303429%'
-		AND i.EPC_Code NOT LIKE 'E28%'
-		AND i.mo_no <> '13D05B006'
-		AND i.stationNO LIKE 'CUS%WH103'
-	UNION ALL
-	SELECT i.EPC_Code, i.po, i.mo_no, i.size_code, i.rfid_status, i.record_time, i.stationNO, i.FC_server_code, r.shoestyle_codefactory, p.mat_ecolor
-	FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily i
-	LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust r
-		ON i.EPC_Code = r.EPC_Code
-	LEFT JOIN wuerp_vnrd.dbo.ta_productmst p
+	LEFT JOIN wuerp_vnrd.dbo.ta_productmst p WITH (NOLOCK)
 		ON p.mat_code = r.mat_code AND p.isactive = 'Y'
 	WHERE
 		i.rfid_status = 'B'
@@ -41,16 +13,8 @@ FROM (
 		AND i.EPC_Code NOT LIKE 'E28%'
 		AND i.mo_no <> '13D05B006'
 		AND i.stationNO LIKE 'CUS%WH103' -- Only select data from the last 2 years
-) AS combined_data;
-
--- * Create index for temporary table to improve query performance
-CREATE INDEX idx_datalist_EPC_Code ON #datalist(EPC_Code);
-CREATE INDEX idx_datalist_po ON #datalist(po);
-CREATE INDEX idx_datalist_mo_no ON #datalist(mo_no);
-CREATE INDEX idx_datalist_shoestyle_codefactory ON #datalist(shoestyle_codefactory);
-CREATE INDEX idx_datalist_mat_ecolor ON #datalist(mat_ecolor);
-
--- * Main stage: Select data
+		AND i.po IS NOT NULL
+)
 SELECT
 	inv.po,
 	inv.shoestyle_codefactory AS shoes_style_code_factory,
@@ -60,12 +24,12 @@ SELECT
 	ac.accumulated_qty,
 	CAST(pl.po_qty - ac.accumulated_qty AS INT) AS missing_qty,
 	pd.detail
-FROM #datalist inv
-LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust rmc
+FROM CTE inv
+LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust rmc WITH (NOLOCK)
 	ON inv.EPC_Code = rmc.EPC_Code
 LEFT JOIN (
 	SELECT po, COUNT(DISTINCT EPC_Code) AS accumulated_qty
-	FROM #datalist
+	FROM CTE
 	GROUP BY po
 ) ac ON ac.po = inv.po
 -- Outstock report master data
@@ -73,7 +37,7 @@ OUTER APPLY (
 	SELECT
 		IIF(ISNULL(a.or_custpoone, '') = '', a.or_custpo, a.or_custpoone) AS po,
 		CAST(SUM(a.or_totalqty) - SUM(a.or_totalcqty) AS INT) AS po_qty
-	FROM wuerp_vnrd.dbo.ta_ordermst a
+	FROM wuerp_vnrd.dbo.ta_ordermst a WITH (NOLOCK)
 	WHERE a.isactive = 'Y' 
 		AND IIF(ISNULL(a.or_custpoone, '') = '', a.or_custpo, a.or_custpoone) = inv.po
 	GROUP BY
@@ -87,17 +51,17 @@ CROSS APPLY (
         SELECT d1.mo_no, (
             -- Stage 2: Select size quantity detail as array JSON
             SELECT
-            d2.size_numcode,
+            d2.size_code AS size_numcode,
             COUNT(DISTINCT d2.EPC_Code) AS qty
-        FROM #datalist d2
+        FROM CTE d2
         WHERE
             d2.po = d1.po
             AND d2.mo_no = d1.mo_no
             AND CAST(d2.record_time AS DATE) = @0
-        GROUP BY d2.size_numcode
+        GROUP BY d2.size_code
         FOR JSON PATH
         ) AS sizes
-    FROM #datalist d1
+    FROM CTE d1
     WHERE 
         d1.po = inv.po 
         AND d1.mat_ecolor = inv.mat_ecolor
@@ -116,6 +80,3 @@ GROUP BY
 	ac.accumulated_qty,
 	pd.detail
 ORDER BY inv.po ASC;
-
--- * Delete temporary table on completion
-DROP TABLE #datalist;
