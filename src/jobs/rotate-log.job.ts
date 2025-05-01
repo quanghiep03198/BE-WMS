d99c29abde9fae56919752336bc3b9915d5c4d98
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-01 20:09:15 +0700

    fix(rotate-log): change cron schedule to run monthly at midnight

--- src/jobs/rotate-log.job.ts
+++ src/jobs/rotate-log.job.ts
@@ -4,8 +4,8 @@ import { Cron, CronExpression } from '@nestjs/schedule'
 
 @Injectable()
 export class RotateLogJob {
-	@Cron(CronExpression.EVERY_WEEKEND, {
-		name: 'DAILY_ROTATE_LOGS'
+	@Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, {
+		name: 'MONTHLY_ROTATE_LOGS'
 	})
 	handleRotateLogs() {
 		FileLogger.rotate()
