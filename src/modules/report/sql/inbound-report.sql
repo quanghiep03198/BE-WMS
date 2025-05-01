d5181d34ec636c430040fffbed1f3a661883dfe5
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-02 00:12:50 +0700

    perf(inbound-report): avoid parameter sniffing and rename CTEs for more readability

--- src/modules/report/sql/inbound-report.sql
+++ src/modules/report/sql/inbound-report.sql
@@ -1,6 +1,7 @@
 DECLARE @FallbackValue NVARCHAR(10) = 'Unknown';
 
-WITH data_src AS (
+-- * Retrieves inbound report data around last 2 years
+WITH filtered_data AS (
 	SELECT EPC_Code, COALESCE(mo_no, @FallbackValue) AS mo_no, COALESCE(size_code, @FallbackValue) AS size_numcode, rfid_status, record_time, stationNO, FC_server_code AS factory_code, ISNULL(dept_name, @FallbackValue) AS dept_name, storage
 	FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily WITH (NOLOCK)
 	WHERE 
@@ -11,28 +12,38 @@ WITH data_src AS (
 		AND stationNO LIKE 'CUS%WH10[12]'
 		AND record_time >= CAST(DATEADD(YEAR, -2, GETDATE()) AS DATE)
 ),
-cmd_det AS (
-	SELECT DISTINCT mo_no, storage, dept_name, factory_code  FROM data_src
+
+-- * Command number details
+command_number_details AS (
+	SELECT DISTINCT mo_no, storage, dept_name, factory_code  FROM filtered_data
 	WHERE storage IS NOT NULL AND dept_name IS NOT NULL
 ),
-dept_ls AS (
+
+-- * Department list of each command number
+department_list AS (
 	SELECT mo_no, factory_code, STRING_AGG(dept_name, ', ') WITHIN GROUP (ORDER BY dept_name) AS dept_name
-	FROM (SELECT DISTINCT dept_name, mo_no, factory_code FROM cmd_det) a
+	FROM (SELECT DISTINCT dept_name, mo_no, factory_code FROM command_number_details) a
 	GROUP BY factory_code, mo_no
 ),
-storage_ls AS (
+
+-- * Storage list of each command number
+storage_list AS (
 	SELECT mo_no, factory_code, STRING_AGG(b.storage_name, ', ') WITHIN GROUP (ORDER BY storage) AS storage_name
-	FROM (SELECT DISTINCT storage, mo_no, factory_code FROM cmd_det) a
+	FROM (SELECT DISTINCT storage, mo_no, factory_code FROM command_number_details) a
 	LEFT JOIN DV_DATA_LAKE.dbo.dv_warehouseccodedet b WITH (NOLOCK) 
 		ON a.storage = b.storage_num
 	GROUP BY factory_code, mo_no
 ),
-acc_counts AS
+
+-- * Accumulated quantity of each command number
+accumulated AS
 (
 	SELECT mo_no, COUNT(DISTINCT EPC_Code) AS accumulated_qty
-	FROM data_src
+	FROM filtered_data
 	GROUP BY mo_no
 )
+
+-- * Main query to retrieve inbound report data
 SELECT
 	ds.factory_code,
 	ds.mo_no,
@@ -45,23 +56,23 @@ SELECT
 	COUNT(DISTINCT ds.EPC_Code) AS daily_inbound_qty,
 	manf.mo_sumqty - ac.accumulated_qty AS missing_qty,
 	sz.size_data
-FROM data_src ds
-LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust rmc WITH (NOLOCK)
+FROM filtered_data ds
+INNER JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust rmc WITH (NOLOCK)
 	ON ds.EPC_Code = rmc.EPC_Code
-LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst manf WITH (NOLOCK)
+INNER JOIN wuerp_vnrd.dbo.ta_manufacturmst manf WITH (NOLOCK)
 	ON manf.mo_no = ds.mo_no
 LEFT JOIN wuerp_vnrd.dbo.ta_productmst prod WITH (NOLOCK)
 	ON rmc.mat_code = prod.mat_code
-LEFT JOIN storage_ls sg
+LEFT JOIN storage_list sg
 	ON sg.mo_no = ds.mo_no AND sg.factory_code = ds.factory_code
-LEFT JOIN dept_ls dg
+LEFT JOIN department_list dg
 	ON dg.mo_no = ds.mo_no AND dg.factory_code = ds.factory_code
-LEFT JOIN acc_counts ac
+LEFT JOIN accumulated ac
 	ON ac.mo_no = ds.mo_no
 OUTER APPLY (
 	SELECT (
 		SELECT size_numcode, COUNT(DISTINCT EPC_Code) AS qty
-		FROM data_src d 
+		FROM filtered_data d 
 		WHERE d.mo_no = ds.mo_no AND CAST(d.record_time AS DATE) = @0
 		GROUP BY size_numcode
 		FOR JSON PATH
@@ -72,3 +83,6 @@ GROUP BY
 	ds.factory_code, ds.mo_no, rmc.shoestyle_codefactory, 
 	prod.mat_ecolor, manf.mo_sumqty, ac.accumulated_qty,
 	sg.storage_name, dg.dept_name, sz.size_data
+	
+-- * Avoid parameter sniffing and set max degree of parallelism;
+OPTION (OPTIMIZE FOR UNKNOWN, MAXDOP 4);
