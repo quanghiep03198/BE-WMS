aeecf6258cbefafd4787018cdfb14df7a66d839e
quanghiep03198 <quanghiep03198@gmail.com>
2025-05-03 00:23:44 +0700

    perf(inventory-report): refactor SQL query to use CTEs for improved readability and performance

--- src/modules/report/sql/inventory-report.sql
+++ src/modules/report/sql/inventory-report.sql
@@ -1,64 +1,88 @@
-SELECT DISTINCT
-   a.inv_yearmonth AS inv_year_month, --month
-   a.brand_name AS brand_name, --Brand
-   a.shoestyle_cofactory AS shoes_style_code_factory, -- Factory Type 工廠型體
-   a.cust_shoestyle, -- 客戶型體
-   a.mo_no,
-   a.inv_type,
-   STUFF(  
-      (
-         SELECT ',' + ISNULL(b.PO, '')
-         FROM DV_DATA_LAKE.dbo.dv_invprodmst B
-         WHERE b.mo_no = A.mo_no
-            AND b.inv_yearmonth = a.inv_yearmonth
-            AND b.brand_name = a.brand_name
-            AND b.shoestyle_cofactory = a.shoestyle_cofactory
-            AND b.mo_no = a.mo_no
-            AND b.inv_type = a.inv_type
-            AND b.cust_shoestyle = a.cust_shoestyle
-            AND ISNULL(b.PO,'') <> ''
-         GROUP BY ISNULL(b.PO, '')
-         FOR XML PATH('')
-      ), 1, 1, ''
-   ) AS po,
-   SUM(a.mo_qty) AS order_qty,  --TOTAL OF mo_qty 指令數量
-   SUM(a.inv_initialqty) AS init_inv_qty, --TOTAL OF Beginning Inventory 期初庫存
-   (SUM(a.inv_istotalqty) + SUM(inv_manualqty)) AS total_instock_qty, -- TOTAL OF Inventory In 入庫
-   (SUM(a.inv_ostotalqty) + SUM(inv_manualqtyout)) AS total_outstock_qty, -- TOTAL OF Inventory Out 出庫
-   (SUM(a.inv_istotalqty) + SUM(inv_manualqty)) - (SUM(a.inv_ostotalqty) + SUM(inv_manualqtyout)) AS actual_inv_qty, -- TOTAL OF Inventory Out 盤盈
-   SUM(a.inv_finalqty) AS final_inv_qty, --TOTAL OF Ending Inventory 期末數量
-   '[' + STUFF(  
-      (
-         SELECT ',' + '{"size":' + + '"' + c.size_numcode + '"' --Size
-            +',"ms_qty":'+ CAST(CAST(max(c.mo_qty) AS FLOAT) AS NVARCHAR(50))+ --指令數量
-            +',"int_qty":' + CAST(CAST(sum(c.inv_initialqty) AS FLOAT) AS NVARCHAR(50)) + --Beginning Inventory 期初庫存
-            +',"ist_qty":' + CAST(CAST(sum(c.inv_istotalqty) AS FLOAT)  AS NVARCHAR(50)) + --Inventory In 入庫
-            +',"mn_ist_qty":' + CAST(CAST(sum(c.inv_manualqty) AS FLOAT)  AS NVARCHAR(50)) + --Inventory In 手填入庫
-            +',"ost_qty":' + CAST(CAST(sum(c.inv_ostotalqty) AS FLOAT) AS NVARCHAR(50)) + --Inventory Out 出庫
-            +',"mn_ost_qty":' + CAST(CAST(sum(c.inv_manualqtyout) AS FLOAT) AS NVARCHAR(50)) + --Inventory Out 手填出庫
-            +',"fnl_qty":' + CAST(CAST(ISNULL(sum(c.inv_finalqty), 0) AS FLOAT) AS NVARCHAR(50)) + --Ending Inventory 期末數量
-            '}'
-         FROM DV_DATA_LAKE.dbo.dv_invprodmst c
-         WHERE c.mo_no = A.mo_no
-            AND c.inv_yearmonth = a.inv_yearmonth
-            AND c.brand_name = a.brand_name
-            AND c.shoestyle_cofactory = a.shoestyle_cofactory
-            AND c.mo_no = a.mo_no
-            AND c.inv_type = a.inv_type
-            AND c.cust_shoestyle = a.cust_shoestyle
-         GROUP BY RIGHT('0000' + IIF(CHARINDEX('.', c.size_numcode) > 0, c.size_numcode, c.size_numcode + '.0'), 5),c.size_numcode
-         ORDER BY RIGHT('0000' + IIF(CHARINDEX('.', c.size_numcode) > 0, c.size_numcode, c.size_numcode + '.0'), 5) ASC
-         FOR XML PATH('')
-      ), 1, 1, '') + 
-   ']' AS size_data
-FROM DV_DATA_LAKE.dbo.dv_invprodmst a
-WHERE a.isactive = 'Y' AND a.inv_type = 'FG' AND a.inv_yearmonth = @0
-GROUP BY
-   a.inv_yearmonth,
-   a.brand_name,
-   a.shoestyle_cofactory,
-   a.mo_no,
-   a.inv_type,
-   a.cust_shoestyle
--- * Avoid parameter sniffing and set max degree of parallelism;
-OPTION (OPTIMIZE FOR UNKNOWN, MAXDOP 4);
+-- CTE để lấy thông tin PO (tối ưu hóa)
+WITH POs AS (
+SELECT 
+	mo_no, 
+	STRING_AGG(po, ',') AS po
+FROM (
+	SELECT DISTINCT po, mo_no 
+	FROM DV_DATA_LAKE.dbo.dv_invprodmst WITH (NOLOCK)
+	WHERE inv_yearmonth = @0
+		AND inv_type = 'FG'
+		AND isactive = 'Y'
+) t
+GROUP BY mo_no
+),
+
+-- CTE chính với dữ liệu tổng hợp (aggregate sớm)
+AggregatedData AS (
+	SELECT
+		mo_no,
+		inv_yearmonth,
+		brand_name,
+		shoestyle_cofactory,
+		cust_shoestyle,
+		inv_type,
+		SUM(mo_qty) AS mo_qty,
+		SUM(inv_initialqty) AS inv_initialqty,
+		SUM(inv_istotalqty) AS inv_istotalqty,
+		SUM(inv_manualqty) AS inv_manualqty,
+		SUM(inv_ostotalqty) AS inv_ostotalqty,
+		SUM(inv_manualqtyout) AS inv_manualqtyout,
+		SUM(inv_finalqty) AS inv_finalqty
+	FROM DV_DATA_LAKE.dbo.dv_invprodmst WITH (NOLOCK)
+	WHERE isactive = 'Y' 
+	AND inv_type = 'FG' 
+	AND inv_yearmonth = @0
+	GROUP BY
+		mo_no,
+		inv_yearmonth,
+		brand_name,
+		shoestyle_cofactory,
+		cust_shoestyle,
+		inv_type
+)
+
+-- Truy vấn chính
+SELECT
+	a.mo_no,
+	a.inv_yearmonth AS inv_year_month,
+	a.brand_name,
+	a.shoestyle_cofactory AS shoes_style_code_factory,
+	a.cust_shoestyle,
+	a.inv_type,
+	TRIM(STUFF(p.po, 1, 1, '')) AS po,
+	CAST(a.mo_qty AS INT) AS order_qty,
+	CAST(a.inv_initialqty AS INT) AS init_inv_qty,
+	CAST(a.inv_istotalqty + a.inv_manualqty AS INT) AS total_instock_qty,
+	CAST(a.inv_ostotalqty + a.inv_manualqtyout AS INT) AS total_outstock_qty,
+	CAST((a.inv_istotalqty + a.inv_manualqty) - (a.inv_ostotalqty + a.inv_manualqtyout) AS INT) AS actual_inv_qty,
+	CAST(a.inv_finalqty AS INT) AS final_inv_qty,
+	(
+		SELECT  
+			c.size_numcode AS size,
+			CAST(MAX(c.mo_qty) AS INT) AS ms_qty,
+			CAST(SUM(c.inv_initialqty) AS INT) AS int_qty,
+			CAST(SUM(c.inv_istotalqty) AS INT) AS ist_qty,
+			CAST(SUM(c.inv_manualqty) AS INT) AS mn_ist_qty,
+			CAST(SUM(c.inv_ostotalqty) AS INT) AS ost_qty,
+			CAST(SUM(c.inv_manualqtyout) AS INT) AS mn_ost_qty,
+			CAST(ISNULL(SUM(c.inv_finalqty), 0) AS INT) AS fnl_qty
+		FROM DV_DATA_LAKE.dbo.dv_invprodmst c WITH (NOLOCK)
+		WHERE c.mo_no = a.mo_no
+			AND c.inv_yearmonth = a.inv_yearmonth
+			AND c.brand_name = a.brand_name
+			AND c.shoestyle_cofactory = a.shoestyle_cofactory
+			AND c.inv_type = a.inv_type
+			AND c.cust_shoestyle = a.cust_shoestyle
+		GROUP BY c.size_numcode
+		ORDER BY 
+			CASE 
+					WHEN CHARINDEX('.', c.size_numcode) > 0 THEN c.size_numcode
+					ELSE c.size_numcode + '.0'
+			END ASC
+		FOR JSON PATH
+	) AS size_data
+FROM AggregatedData a
+INNER JOIN POs p ON p.mo_no = a.mo_no
+ORDER BY a.mo_no DESC
+OPTION (OPTIMIZE FOR UNKNOWN, MAXDOP 8, FAST 100);
