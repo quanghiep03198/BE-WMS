42517e8974e19e8b6378a85e5b1a0291e0881b6c
quanghiep03198 <quanghiep03198@gmail.com>
2025-07-27 18:55:33 +0700

    docs(database): mongodb replica set installation

--- docs/@database/mongo.ini.sh
+++ docs/@database/mongo.ini.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-mongosh <<\EOF
+BASE_PATH=;
+HOST=;
+
+mongod --replSet rs0 --bind_ip_all --port 27017 --dbpath "$BASE_PATH\27017\data";
+mongod --replSet rs0 --bind_ip_all --port 27018 --dbpath "$BASE_PATH\27018\data";
+mongod --replSet rs0 --bind_ip_all --port 27019 --dbpath "$BASE_PATH\27019\data";
+
+mongosh --host $HOST <<\EOF
 var config = {
     _id: "rs0",
     version: 1,
     members: [
         {
             _id: 1,
-            host: "0.0.0.0:27017",
+            host: "$HOST:27017",
             priority: 3
         },
         {
             _id: 2,
-            host: "0.0.0.0:27018",
+            host: "$HOST:27018",
             priority: 2
         },
         {
             _id: 3,
-            host: "0.0.0.0:27019",
+            host: "$HOST:27019",
             priority: 1
         }
     ]
--- docs/@database/mongo.service.bat
+++ docs/@database/mongo.service.bat
@@ -1,9 +1,8 @@
+set MONGO_BASE_PATH=
 
-
-
-mongod --config "C:\Program Files\MongoDB\Server\8.0\rs0\27017\bin\mongod.cfg" --serviceName mongo-primary --serviceDisplayName "MongoDB Primary" --install
-mongod --config "C:\Program Files\MongoDB\Server\8.0\rs0\27018\bin\mongod.cfg" --serviceName mongo-secondary-18 --serviceDisplayName "MongoDB Secondary 1" --install
-mongod --config "C:\Program Files\MongoDB\Server\8.0\rs0\27019\bin\mongod.cfg" --serviceName mongo-secondary-19 --serviceDisplayName "MongoDB Secondary 2" --install
+mongod --config "%MONGO_BASE_PATH%/27017/bin/mongod.conf" --serviceName mongo-primary --serviceDisplayName "MongoDB Primary" --install
+mongod --config "%MONGO_BASE_PATH%/27018/bin/mongod.conf" --serviceName mongo-secondary-18 --serviceDisplayName "MongoDB Secondary 1" --install
+mongod --config "%MONGO_BASE_PATH%/27019/bin/mongod.conf" --serviceName mongo-secondary-19 --serviceDisplayName "MongoDB Secondary 2" --install
 
 net start mongo-primary
 net start mongo-secondary-18
