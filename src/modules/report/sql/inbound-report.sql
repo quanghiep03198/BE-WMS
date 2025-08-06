cb8fcb7ec17662df072fe6b7cce3838caacf8cf7
quanghiep03198 <quanghiep03198@gmail.com>
2025-08-06 15:16:42 +0700

    refactor(report): streamline SQL query structure and improve readability in inbound report

--- src/modules/report/sql/inbound-report.sql
+++ src/modules/report/sql/inbound-report.sql
@@ -1,91 +1,92 @@
-SET STATISTICS TIME, IO ON;
-
 DECLARE @FallbackValue NVARCHAR(10) = 'Unknown';
 
 -- * Retrieves inbound report data around last 2 years
-WITH filtered_data AS (
+WITH rfid_inbound_cte AS (
 	SELECT EPC_Code, COALESCE(mo_no, @FallbackValue) AS mo_no, COALESCE(size_code, @FallbackValue) AS size_numcode, rfid_status, record_time, stationNO, FC_server_code AS factory_code, ISNULL(dept_name, @FallbackValue) AS dept_name, storage
-	FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily WITH (READUNCOMMITTED)
+	FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily WITH (NOLOCK)
 	WHERE 
 		rfid_status = 'A'
-		AND record_time >= CAST(DATEADD(YEAR, -2, GETDATE()) AS DATE)
-		AND EPC_Code NOT LIKE '303429%'
-		AND EPC_Code NOT LIKE 'E28%'
-		AND mo_no NOT IN ('13D05B006', '13A08C003')
 		AND stationNO LIKE 'CUS%WH10[12]'
 		AND FC_server_code = @0
+		AND mo_no NOT IN ('13D05B006', '13A08C003')
+		AND EPC_Code NOT LIKE '303429%'
+		AND EPC_Code NOT LIKE 'E28%'
+		AND record_time >= CAST(DATEADD(YEAR, -2, GETDATE()) AS DATE)
 ),
 
 -- * Command number details
-command_number_details AS (
-	SELECT DISTINCT mo_no, storage, dept_name, factory_code  FROM filtered_data
-	WHERE storage IS NOT NULL AND dept_name IS NOT NULL
+inbound_detail_cte AS (
+	SELECT DISTINCT mo_no, storage, dept_name, factory_code  FROM rfid_inbound_cte
+	WHERE 
+	storage IS NOT NULL 
+	AND dept_name IS NOT NULL
+	AND CAST(record_time AS DATE) = @1
 ),
 
 -- * Department list of each command number
-department_list AS (
+department_list_cte AS (
 	SELECT mo_no, factory_code, STRING_AGG(dept_name, ', ') WITHIN GROUP (ORDER BY dept_name) AS dept_name
-	FROM (SELECT DISTINCT dept_name, mo_no, factory_code FROM command_number_details) a
+	FROM (SELECT DISTINCT dept_name, mo_no, factory_code FROM inbound_detail_cte) a
+	WHERE dept_name IS NOT NULL
 	GROUP BY factory_code, mo_no
 ),
 
 -- * Storage list of each command number
-storage_list AS (
-	SELECT mo_no, factory_code, STRING_AGG(b.storage_name, ', ') WITHIN GROUP (ORDER BY storage) AS storage_name
-	FROM (SELECT DISTINCT storage, mo_no, factory_code FROM command_number_details) a
+storage_list_cte AS (
+	SELECT mo_no, factory_code, STRING_AGG(b.storage_name, ', ') WITHIN GROUP (ORDER BY storage ASC) AS storage_name
+	FROM (SELECT DISTINCT storage, mo_no, factory_code FROM inbound_detail_cte) a
 	LEFT JOIN DV_DATA_LAKE.dbo.dv_warehouseccodedet b
 		ON a.storage = b.storage_num
 	GROUP BY factory_code, mo_no
 ),
 
 -- * Accumulated quantity of each command number
-accumulated AS
+accumulated_cte AS
 (
 	SELECT mo_no, COUNT(DISTINCT EPC_Code) AS accumulated_qty
-	FROM filtered_data
+	FROM rfid_inbound_cte
 	GROUP BY mo_no
 )
 
 -- * Main query to retrieve inbound report data
 SELECT
-	ds.factory_code,
-	ds.mo_no,
+	ric.factory_code,
+	ric.mo_no,
 	COALESCE(rmc.shoestyle_codefactory,@FallbackValue) AS factory_shoes_style,
 	COALESCE(prod.color_sn, @FallbackValue) AS color_sn,
-	dg.dept_name AS shaping_dept_name,
-	sg.storage_name AS storage,
+	dlc.dept_name AS shaping_dept_name,
+	slc.storage_name AS storage,
 	CAST(COALESCE(manf.mo_totalqty, 0) AS INT) AS order_qty,
 	ac.accumulated_qty,
-	COUNT(DISTINCT ds.EPC_Code) AS daily_inbound_qty,
+	COUNT(DISTINCT ric.EPC_Code) AS daily_inbound_qty,
 	CAST((manf.mo_totalqty - ac.accumulated_qty) AS INT) AS missing_qty,
 	(
 		SELECT size_numcode, COUNT(DISTINCT EPC_Code) AS qty
-		FROM filtered_data d 
-		WHERE d.mo_no = ds.mo_no AND CAST(d.record_time AS DATE) = @1
+		FROM rfid_inbound_cte d 
+		WHERE d.mo_no = ric.mo_no AND CAST(d.record_time AS DATE) = @1
 		GROUP BY size_numcode
 		FOR JSON PATH
 	) AS size_data
-FROM filtered_data ds
-LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust rmc WITH (FORCESEEK)
-	ON ds.EPC_Code = rmc.EPC_Code
-LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst manf WITH (FORCESEEK)
-	ON manf.mo_no = ds.mo_no
+FROM rfid_inbound_cte ric
+LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust rmc WITH(FORCESEEK)
+	ON ric.EPC_Code = rmc.EPC_Code
+LEFT JOIN wuerp_vnrd.dbo.ta_manufacturmst manf WITH(FORCESEEK)
+	ON manf.mo_no = ric.mo_no
 LEFT JOIN wuerp_vnrd.dbo.ta_productmst prod WITH (FORCESEEK)
 	ON rmc.mat_code = prod.mat_code
-LEFT JOIN storage_list sg 
-	ON sg.mo_no = ds.mo_no AND sg.factory_code = ds.factory_code
-LEFT JOIN department_list dg 
-	ON dg.mo_no = ds.mo_no AND dg.factory_code = ds.factory_code
-LEFT JOIN accumulated ac 
-	ON ac.mo_no = ds.mo_no
-WHERE CAST(ds.record_time AS DATE) = @1
+LEFT JOIN storage_list_cte slc 
+	ON slc.mo_no = ric.mo_no AND slc.factory_code = ric.factory_code
+LEFT JOIN department_list_cte dlc 
+	ON dlc.mo_no = ric.mo_no AND dlc.factory_code = ric.factory_code
+LEFT JOIN accumulated_cte ac 
+	ON ac.mo_no = ric.mo_no
+WHERE CAST(ric.record_time AS DATE) = @1
 GROUP BY 
-	ds.factory_code, ds.mo_no, rmc.shoestyle_codefactory, 
+	ric.factory_code, ric.mo_no, rmc.shoestyle_codefactory, 
 	prod.color_sn, manf.mo_totalqty, ac.accumulated_qty,
-	sg.storage_name, dg.dept_name
-ORDER BY ds.mo_no DESC
+	slc.storage_name, dlc.dept_name
+ORDER BY ric.mo_no DESC
 OPTION (
-	OPTIMIZE FOR UNKNOWN,	-- * Avoid "Parameter Sniffing" issues
-	MAXDOP 8,					-- * Use server's max degree of parallelism
-	RECOMPILE					-- * Re-optimize for each execution
+	OPTIMIZE FOR UNKNOWN,                        		-- * Avoid "Parameter Sniffing" issues
+	RECOMPILE                                         	-- * Re-optimize for each execution
 );
