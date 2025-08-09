9099f9327286e6d63e1170e3343919133c41b4c9
quanghiep03198 <quanghiep03198@gmail.com>
2025-08-09 16:29:17 +0700

    feat(event-gateway): integrate PinoLogger for improved logging and refine EPC data retrieval logic

--- src/events/event.gateway.ts
+++ src/events/event.gateway.ts
@@ -7,7 +7,7 @@ import { EpcDocument, EpcInbound } from '@/modules/rfid/schemas/epc.schema'
 import { THIRD_PARTY_API_SYNC } from '@/modules/third-party-api/constants'
 import { SyncDataMessageDTO, syncDataMessageValidator } from '@/modules/third-party-api/dto/third-party-api.dto'
 import { InjectQueue } from '@nestjs/bullmq'
-import { Logger, Optional, UseFilters, UsePipes } from '@nestjs/common'
+import { Optional, UseFilters, UsePipes } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import {
 	MessageBody,
@@ -20,16 +20,17 @@ import {
 import { Queue } from 'bullmq'
 import { uniqBy, uniqueId } from 'lodash'
 import { PaginateModel } from 'mongoose'
+import { PinoLogger } from 'nestjs-pino'
 import { Socket } from 'socket.io'
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class EventGateway implements OnGatewayConnection, OnGatewayDisconnect {
-	private readonly logger = new Logger(EventGateway.name)
-
 	@WebSocketServer()
 	server: Socket
 
 	constructor(
+		private readonly logger: PinoLogger,
+
 		@Optional()
 		@InjectModel(EpcInbound.name)
 		private readonly epcModel: PaginateModel<EpcDocument>,
@@ -43,12 +44,11 @@ export class EventGateway implements OnGatewayConnection, OnGatewayDisconnect {
 		private readonly syncInventoryAuditDataQueue: Queue<SyncDataMessageDTO>
 	) {}
 
-	public handleConnection(socket: Socket) {
-		this.logger.debug(socket.handshake.headers.authorization)
-	}
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	public handleConnection(_: Socket) {}
 
 	public handleDisconnect(client: Socket) {
-		this.logger.log(`Client disconnected: ${client.id}`)
+		this.logger.info(`Client disconnected: ${client.id}`)
 	}
 
 	@SubscribeMessage('sync_decker_data')
@@ -56,11 +56,18 @@ export class EventGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	@UsePipes(new WsZodValidationPipe(syncDataMessageValidator))
 	protected async onSyncDeckerData(@MessageBody() payload: SyncDataMessageDTO) {
 		if (!this.syncThirdPartyApiDataQueue) return
-		const validUnknownEpcs = await this.epcModel.find({ mo_no: FALLBACK_VALUE }).lean(true)
-		const jobData = uniqBy(validUnknownEpcs, (item) => item.epc.substring(0, 22)).map((item) => item.epc)
+		const validUnknownEpcs = await this.epcModel
+			.distinct('epc', {
+				scannable: true,
+				mo_no: FALLBACK_VALUE,
+				epc: { $regex: /^3034(?!29)/ }
+			})
+			.lean(true)
+		const jobData = uniqBy(validUnknownEpcs, (item) => item.substring(0, 22))
 		this.syncThirdPartyApiDataQueue.add(payload.id, jobData, {
 			jobId: payload.factory,
-			removeOnComplete: true
+			removeOnComplete: true,
+			removeOnFail: true
 		})
 	}
 
