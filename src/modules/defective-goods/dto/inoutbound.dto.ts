830c7f5c92722a2fe3a1bd56dd7821facf99324b
quanghiep03198 <quanghiep03198@gmail.com>
2025-11-11 16:27:22 +0700

    fix(defective-goods): change inbound and outbound date fields to use date type for consistency

--- src/modules/defective-goods/dto/inoutbound.dto.ts
+++ src/modules/defective-goods/dto/inoutbound.dto.ts
@@ -4,7 +4,7 @@ import { DefectiveGoodsOutboundPurpose } from '../constants'
 export const updateInboundStatusDTO = z.object({
 	epcs: z.array(z.string().trim().nonempty()),
 	storage_location: z.string().trim().nonempty(),
-	inbound_date: z.date().default(new Date())
+	inbound_date: z.coerce.date().default(new Date())
 })
 
 export const updateOutboundStatusDTO = z.object({
@@ -12,7 +12,7 @@ export const updateOutboundStatusDTO = z.object({
 	outbound_purpose: z.nativeEnum(DefectiveGoodsOutboundPurpose, {
 		message: `Outbound purpose must be one of: ${Object.values(DefectiveGoodsOutboundPurpose).join(', ')}`
 	}),
-	outbound_date: z.date().default(new Date())
+	outbound_date: z.coerce.date().default(new Date())
 })
 
 export type UpdateInboundStatusDTO = z.infer<typeof updateInboundStatusDTO>
