c0aa1ca1ca4823fb1415651bc515c2b76a5f7005
quanghiep03198 <quanghiep03198@gmail.com>
2025-11-24 15:28:42 +0700

    feat(defective-goods): update storage_location and remove outbound_date from DTOs

--- src/modules/defective-goods/dto/inoutbound.dto.ts
+++ src/modules/defective-goods/dto/inoutbound.dto.ts
@@ -3,16 +3,18 @@ import { DefectiveGoodsOutboundPurpose } from '../constants'
 
 export const updateInboundStatusDTO = z.object({
 	epcs: z.array(z.string().trim().nonempty()),
-	storage_location: z.string().trim().nonempty(),
-	inbound_date: z.coerce.date().default(new Date())
+	storage_location: z
+		.string()
+		.trim()
+		.nonempty()
+		.transform((value) => value.toUpperCase())
 })
 
 export const updateOutboundStatusDTO = z.object({
 	epcs: z.array(z.string().trim().nonempty()),
 	outbound_purpose: z.nativeEnum(DefectiveGoodsOutboundPurpose, {
 		message: `Outbound purpose must be one of: ${Object.values(DefectiveGoodsOutboundPurpose).join(', ')}`
-	}),
-	outbound_date: z.coerce.date().default(new Date())
+	})
 })
 
 export type UpdateInboundStatusDTO = z.infer<typeof updateInboundStatusDTO>
