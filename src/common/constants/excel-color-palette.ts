cadab3c609b0b38a582de43511443224d5adfb78
quanghiep03198 <quanghiep03198@gmail.com>
2025-12-04 00:35:17 +0700

    feat: add BG_LIGHT_RED color to ExcelColorPalette

--- src/common/constants/excel-color-palette.ts
+++ src/common/constants/excel-color-palette.ts
@@ -2,6 +2,7 @@ export enum ExcelColorPalette {
 	BG_LIGHT_YELLOW = 'fff2cc',
 	BG_LIGHT_BLUE = 'deecf7',
 	BG_LIGHT_NEUTRAL = 'e5e5e5',
+	BG_LIGHT_RED = 'f2dcdb',
 	FOREGROUND = '171717',
 	DESTRUCTIVE_FOREGROUND = 'ff0000',
 	BORDER = 'a1a1a1'
