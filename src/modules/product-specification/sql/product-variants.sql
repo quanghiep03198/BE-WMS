a8d3ccaba281e784413fa87509771869cc301cee
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-13 11:26:01 +0700

    fix(product-variants): extend date filter to include records from the last 5 years

--- src/modules/product-specification/sql/product-variants.sql
+++ src/modules/product-specification/sql/product-variants.sql
@@ -74,7 +74,7 @@ CROSS APPLY (
             AND sz.size_numcode IS NOT NULL
             AND sz.size_qty > 0
             AND p.shoestyle_systemcodefty = a.shoestyle_systemcodefty
-            AND m.created >= CAST(DATEADD(YEAR, -3, GETDATE()) AS DATE)
+            AND m.created >= CAST(DATEADD(YEAR, -5, GETDATE()) AS DATE)
          FOR JSON PATH
       ) AS sizes
    ) s (sizes)
@@ -86,7 +86,7 @@ CROSS APPLY (
 ) cl (product_variants)
 WHERE a.isactive = 'Y' 
 AND cl.product_variants IS NOT NULL 
-AND a.created >= CAST(DATEADD(YEAR, -3, GETDATE()) AS DATE)
+AND a.created >= CAST(DATEADD(YEAR, -5, GETDATE()) AS DATE)
 AND c.brand_code IN ('UG', 'TV', 'KB') 
 ORDER BY brand_name ASC
 OPTION (
