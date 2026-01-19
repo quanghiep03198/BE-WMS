6b1f3fcdbce9c9acd729d49087ee380ea9920d0d
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-19 16:39:25 +0700

    feat(user-entity): create UserEntity with role and authorized factory codes

---
+++ src/databases/migrations/1768439528524-users.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Users1768439528524 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
