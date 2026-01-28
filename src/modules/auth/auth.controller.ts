ba4a9b536f7cb171c3dc8a1337419cb6f949b3d5
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-28 22:55:33 +0700

    refactor(auth): improve token handling and error management in AuthService and AuthController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { HttpMethod, RequireAuthenticated, RouteHandler, User } from '@/common/decorators'
+import { HttpMethod, RequestUser, RequireAuthenticated, RouteHandler, User } from '@/common/decorators'
 // import { Cookies } from '@/common/decorators/cookies.decorator'
 import { CommonRequestHeader } from '@/common/constants'
 import { Cookies } from '@/common/decorators/cookies.decorator'
@@ -16,19 +16,19 @@ export class AuthController {
 		method: HttpMethod.POST
 	})
 	@UseGuards(LocalAuthGuard)
-	async login(@User() user: any, @Res({ passthrough: true }) res: FastifyReply) {
+	async login(@User() user: RequestUser, @Res({ passthrough: true }) res: FastifyReply) {
 		const data = await this.authService.login(user)
 		res.setCookie('refresh-token', data.refreshToken, {
 			maxAge: 30 * 24 * 60 * 60,
 			httpOnly: true,
-			sameSite: 'strict',
+			sameSite: 'lax',
 			path: '/',
 			secure: 'auto'
 		})
 		res.setCookie('access-token', data.accessToken, {
-			maxAge: 60,
+			maxAge: 60 * 5,
 			httpOnly: true,
-			sameSite: 'strict',
+			sameSite: 'lax',
 			path: '/',
 			secure: 'auto'
 		})
@@ -41,29 +41,34 @@ export class AuthController {
 	})
 	async refreshToken(
 		@Cookies('refresh-token') refreshToken: string,
-		@Headers(CommonRequestHeader.USER) username: string,
+		@Headers(CommonRequestHeader.USER_REQUEST) username: string,
 		@Res({ passthrough: true }) res: FastifyReply
 	): Promise<string> {
-		console.log('\n\n\nusername', username, '\n\n\n')
-		const { newAccessToken, newRefreshToken } = await this.authService.refreshToken(username, refreshToken)
+		try {
+			const { newAccessToken, newRefreshToken } = await this.authService.refreshToken(username, refreshToken)
 
-		res.setCookie('access-token', newAccessToken, {
-			maxAge: 60 * 60,
-			httpOnly: true,
-			sameSite: 'strict',
-			path: '/',
-			secure: 'auto'
-		})
+			res.setCookie('access-token', newAccessToken, {
+				maxAge: 60 * 5,
+				httpOnly: true,
+				sameSite: 'lax',
+				path: '/',
+				secure: 'auto'
+			})
 
-		res.setCookie('refresh-token', newRefreshToken, {
-			maxAge: 30 * 24 * 60 * 60,
-			httpOnly: true,
-			sameSite: 'strict',
-			path: '/',
-			secure: 'auto'
-		})
+			res.setCookie('refresh-token', newRefreshToken, {
+				maxAge: 30 * 24 * 60 * 60,
+				httpOnly: true,
+				sameSite: 'lax',
+				path: '/',
+				secure: 'auto'
+			})
 
-		return newAccessToken
+			return newAccessToken
+		} catch (error) {
+			res.clearCookie('access-token', { path: '/', httpOnly: true, sameSite: 'strict' })
+			res.clearCookie('refresh-token', { path: '/', httpOnly: true, sameSite: 'strict' })
+			throw error
+		}
 	}
 
 	@RouteHandler({
@@ -72,8 +77,8 @@ export class AuthController {
 	})
 	@RequireAuthenticated()
 	async logout(@User('username') username: string, @Res({ passthrough: true }) res: FastifyReply) {
-		res.clearCookie('access-token', { path: '/', httpOnly: true, sameSite: 'strict', secure: 'auto' })
-		res.clearCookie('refresh-token', { path: '/', httpOnly: true, sameSite: 'strict', secure: 'auto' })
+		res.clearCookie('access-token', { path: '/', httpOnly: true, sameSite: 'lax', secure: 'auto' })
+		res.clearCookie('refresh-token', { path: '/', httpOnly: true, sameSite: 'lax', secure: 'auto' })
 
 		return await this.authService.logout(username)
 	}
