db6e29ae928451efd71f62f501be64d5c3a8c5ee
quanghiep03198 <quanghiep03198@gmail.com>
2026-01-30 16:36:03 +0700

    refactor(tenancy): add RequireAuthenticated decorator to getAll and getByFactory methods

--- src/modules/tenancy/tenancy.controller.ts
+++ src/modules/tenancy/tenancy.controller.ts
@@ -1,5 +1,5 @@
 import { CommonRequestHeader } from '@/common/constants'
-import { HttpMethod, RouteHandler } from '@/common/decorators'
+import { HttpMethod, RequireAuthenticated, RouteHandler } from '@/common/decorators'
 import { BadRequestException, Controller, Headers } from '@nestjs/common'
 import { FactoryCode } from '../department/constants'
 import { TenancyService } from './tenancy.service'
@@ -9,11 +9,13 @@ export class TenancyController {
 	constructor(private readonly tenancyService: TenancyService) {}
 
 	@RouteHandler({ method: HttpMethod.GET })
+	@RequireAuthenticated()
 	getAll() {
 		return this.tenancyService.getAll()
 	}
 
 	@RouteHandler({ endpoint: 'by-factory', method: HttpMethod.GET })
+	@RequireAuthenticated()
 	getByFactory(@Headers(CommonRequestHeader.FACTORY_CODE) factoryCode: FactoryCode) {
 		if (!factoryCode) throw new BadRequestException('Please provide factory code')
 		return this.tenancyService.getByFactory(factoryCode)
