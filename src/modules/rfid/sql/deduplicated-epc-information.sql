cc53fb8d50d3f43bfa04df6b3246d013beff3c38
quanghiep03198 <quanghiep03198@gmail.com>
2026-04-15 15:45:50 +0700

    feat(rfid): add SQL script for deduplicating EPC information

---
+++ src/modules/rfid/sql/deduplicated-epc-information.sql
@@ -0,0 +1,26 @@
+DECLARE @FallbackValue NVARCHAR(10) = 'Unknown';
+
+WITH inbound_epcs AS (
+   SELECT EPC_Code FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet
+   UNION ALL
+   SELECT EPC_Code FROM DV_DATA_LAKE.dbo.dv_InvRFIDrecorddet_backup_Daily
+)
+SELECT DISTINCT a.EPC_Code AS epc, 
+   ISNULL(b.mo_no, @FallbackValue) AS mo_no,
+   COALESCE(b.shoestyle_codefactory, @FallbackValue) AS factory_shoes_style,
+   COALESCE(c.color_sn, @FallbackValue) AS color_sn,
+   COALESCE(b.size_numcode, @FallbackValue) AS size_numcode,
+   b.factory_code_produce
+FROM (SELECT value AS EPC_Code FROM STRING_SPLIT(CAST(@0 AS NVARCHAR(MAX)), ',')) AS a
+LEFT JOIN DV_DATA_LAKE.dbo.dv_rfidmatchmst_cust b ON a.EPC_Code = b.EPC_Code
+LEFT JOIN wuerp_vnrd.dbo.ta_productmst c ON b.mat_code = c.mat_code
+WHERE 
+   a.EPC_Code NOT LIKE '303429%'
+   AND LEN(a.EPC_Code) = 24
+   AND (
+      b.mo_no IS NULL 
+      OR b.mo_no NOT IN (SELECT value AS mo_no FROM STRING_SPLIT(@1, ','))
+   )
+   AND a.EPC_Code NOT IN (
+      SELECT DISTINCT EPC_Code FROM inbound_epcs
+   )
