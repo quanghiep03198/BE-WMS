ef982e0f00537c0603de7f29005f1adc5b98d7b7
quanghiep03198 <quanghiep03198@gmail.com>
2026-07-02 00:31:22 +0700

    refactor(delete-scanning-epcs): change command return type from number to void

--- src/modules/inoutbound/application/commands/delete-scanning-epcs/delete-scanning-epcs.command.ts
+++ src/modules/inoutbound/application/commands/delete-scanning-epcs/delete-scanning-epcs.command.ts
@@ -1,7 +1,7 @@
 import { InventoryAction } from '@/modules/inoutbound/domain/types'
 import { Command } from '@nestjs/cqrs'
 
-export class DeleteScanningEpcsCommand extends Command<number> {
+export class DeleteScanningEpcsCommand extends Command<void> {
 	constructor(
 		public readonly inventoryAction: InventoryAction,
 		public readonly scanningEpcs: Array<string>,
